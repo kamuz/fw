3af519d730f2f7a8d7022c4a9255cb61cda31593
Vladimir Kamuz <v.kamuz@gmail.com>
2022-09-19 19:29:39 +0300

    Added class Registry

--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
 <?php
 
 require_once dirname(__DIR__) . '/config/init.php';
+require_once LIBS . '/functions.php';
+
+$shop = new \ishop\App();
+$shop::$app->setProperty('test', 'Test');
+debug($shop::$app->getProperties());
---
+++ vendor/ishop/core/App.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ishop;
+
+class App{
+
+	public static $app;
+
+	public function __construct(){
+		$query = trim($_SERVER['QUERY_STRING'], '/');
+		session_start();
+		// Singleton instance
+		self::$app = Registry::instance();
+		// Get params init
+		$this->getParams();
+	}
+
+	// Loop params from file and set as object properties
+	protected function getParams(){
+		$params = require_once CONF . '/params.php';
+		if(!empty($params)){
+			foreach($params as $key => $value){
+				self::$app->setProperty($key, $value);
+			}
+		}
+	}
+}
