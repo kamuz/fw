3516ca8de5c1bbd7b176dc4b144a8e85a83007fe
Vladimir Kamuz <v.kamuz@gmail.com>
2022-09-27 00:41:00 +0300

    Router done

---
+++ app/controllers/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\controllers;
+
+class PageController{
+
+	public function indexAction(){
+		echo __METHOD__;
+	}
+
+	public function viewAction(){
+		echo __METHOD__;
+	}
+}
--- vendor/ishop/core/Router.php
+++ vendor/ishop/core/Router.php
@@ -27,14 +27,67 @@ class Router {
 	// Work with URL
 	public static function dispatch( $url ) {
 		if ( self::matchRoute( $url ) ) {
-			echo 'Okay';
+			// Set path to controller
+			$controller = 'app\controllers\\' . self::$route['prefix'] . self::$route['controller'] . 'Controller';
+			if ( class_exists( $controller ) ) {
+				// Create object controller and passed route
+				$controllerObject = new $controller( self::$route );
+				// Get current action
+				$action = self::lowerCamelCase( self::$route['action'] ) . 'Action';
+				if ( method_exists( $controllerObject, $action ) ) {
+					// Call action
+					$controllerObject->$action();
+				} else {
+					throw new \Exception( "Method $controller::$action not found" );
+				}
+			} else {
+				throw new \Exception( "Controller $controller not found" );
+			}
 		} else {
-			echo 'Nope';
+			// Route not found
+			throw new \Exception( "Page not found", 404 );
 		}
 	}
 
-	// Search matches
+	// Search matched route, compare query string and route or regular expression
 	public static function matchRoute( $url ) {
+		foreach ( self::$routes as $pattern => $route ) {
+			if ( preg_match( "#${pattern}#", $url, $matches ) ) {
+				// Get only items of array with string keys
+				foreach( $matches as $key => $value ) {
+					if ( is_string( $key ) ) {
+						$route[ $key ] = $value;
+					}
+				}
+				// If route empty - set default action controller
+				if ( empty( $route['action'] ) ) {
+					$route['action'] = 'index';
+				}
+				// Add empty prefix by default or add additional backslash
+				if ( empty( $route['prefix'] ) ) {
+					$route['prefix'] = '';
+				} else {
+					$route['prefix'] .= '\\';
+				}
+				$route['controller'] = self::upperCamelCase( $route['controller'] );
+				// Set current route
+				self::$route = $route;
+				debug( self::$route );
+				return true;
+			}
+		}
 		return false;
+	}
+
+	// For controllers, example - CamelCase
+	protected static function upperCamelCase( $name ) {
+		// From page-new to PageNew
+		$name = str_replace( ' ', '', ucwords( str_replace( '-', ' ', $name ) ) );
+		return $name;
+	}
+
+	// For actions, example - camelCase
+	protected static function lowerCamelCase( $name ) {
+		return lcfirst( self::upperCamelCase( $name ) );
 	}
 }
